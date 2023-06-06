ede298208138a09015c4ef53aad79909242223de
kbl91 <kbl91@yahoo.com>
2023-06-06 15:09:14 -0400

    install PUG package

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+
+const server = app.listen(port, () => console.log(" Server listening on port " + port));
+
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res, next) => { 
+    res.status(200).send("Yay");
+})
