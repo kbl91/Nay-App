c16f6a16f2210f5fefcdc5b97d504e89badec6bd
kbl91 <kbl91@yahoo.com>
2023-06-06 15:51:41 -0400

    create middleware

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const middleware = require('./middleware')
 
 const server = app.listen(port, () => console.log(" Server listening on port " + port));
 
 
 app.set('view engine', 'pug');
+app.set('views', 'views');
 
-app.get('/', (req, res, next) => { 
-    res.status(200).send("Yay");
+app.get('/', middleware.requireLogin, (req, res, next) => {
+
+    var payload = {
+        pageTitle: "Home"
+    }
+
+    res.status(200).render('home', payload);
 })
