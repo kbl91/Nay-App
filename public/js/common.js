ee4cd3c155d2acd9191ecd6f46ab29b29d1c7e41
kbl91 <kbl91@yahoo.com>
2023-06-08 16:16:57 -0400

    enable and disable 'post' button

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,14 @@
-$(document).ready(() => {
+$("#postTextarea").keyup(event => {
+    var textbox = $(event.target);
+    var value = textbox.val().trim();
     
+    var submitButton = $("#submitPostButton");
+
+    if (submitButton.length == 0) return alert("no submit button found");
+
+    if (value == "") {
+        submitButton.prop("disabled", true);
+        return;
+    }
+    submitButton.prop("disabled", false);
 })
